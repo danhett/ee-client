3018dfdeb0b45e87d1d54166f027a78b9388d50b
Dan Hett <hellodanhett@gmail.com>
2017-06-02 14:41:05 +0100

    Temp update for signs

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import urllib2
 import json
+from urllib import quote
+
+print "Getting poem..."
 
 url = 'http://everythingeverytime.herokuapp.com/poem'
 
@@ -7,5 +10,14 @@ httpreq = urllib2.urlopen(url)
 response = httpreq.read()
 poem = json.loads(response)
 
-for i in poem['poem']:
-    print i
+#for i in poem['poem']:
+#    print i
+
+
+line = quote(poem["poem"][0], safe='')
+print "Sending line: " + line
+
+sendURL = 'http://192.168.125.47/small/2/2/' + line
+
+signReq = urllib2.urlopen(sendURL)
+signReq.read()
