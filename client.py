5bca8274c53b3b0661804d57a10f036711c36996
Dan Hett <hellodanhett@gmail.com>
2017-06-05 19:48:47 +0100

    Clarified

--- client.py
+++ client.py
@@ -1,25 +1,37 @@
+"""
+<every thing, every time>
+client.py
+
+@author Dan Hett (hellodanhett@gmail.com)
+
+This is a small python script designed to manage the signs for Naho Matsuda's
+'every thing, every time' artwork for CityVerve. Specifically, it handles
+timed queries to the API to grab poems, and then outputs the lines to the
+displays, timed appropriately to work correctly with the hardware.
+"""
 import urllib2
 import json
 import time
 import sched
 from urllib import quote
 
-# Set this to use localhost
-isLocal = True
-
-poemRequestTime = 5
-
+# settings
+isLocal = True  # sets localhost/remote
+poemRequestTime = 5 # number of seconds between poem requests
 remoteurl = 'http://everythingeverytime.herokuapp.com/poem'
 testurl = 'http://localhost:8080/poem'
 
+# setup
 if isLocal:
     url = testurl
 else:
     url = remoteurl
 
-# Creates a scheduler to make our API calls
+
+# create scheduler
 s = sched.scheduler(time.time, time.sleep)
 
+# makes the poem request
 def getPoem(sc):
     print "Getting poem..."
 
@@ -36,7 +48,7 @@ def getPoem(sc):
 s.enter(poemRequestTime, 1, getPoem, (s,))
 s.run()
 
-
+# TODO - this will output to the signs
 #sendURL = 'http://192.168.125.47/small/2/2/' + line
 #signReq = urllib2.urlopen(sendURL)
 #signReq.read()
