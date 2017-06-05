1b0da398ccd9be51cfa8511f791d5e4ce871623c
Dan Hett <hellodanhett@gmail.com>
2017-06-05 19:30:38 +0100

    Made a scheduler for poem requests

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import urllib2
 import json
+import time
+import sched
 from urllib import quote
 
 # Set this to use localhost
 isLocal = True
+
+poemRequestTime = 5
 
 remoteurl = 'http://everythingeverytime.herokuapp.com/poem'
 testurl = 'http://localhost:8080/poem'
@@ -13,18 +17,26 @@ if isLocal:
 else:
     url = remoteurl
 
-httpreq = urllib2.urlopen(url)
-response = httpreq.read()
-poem = json.loads(response)
+# Creates a scheduler to make our API calls
+s = sched.scheduler(time.time, time.sleep)
 
-#for i in poem['poem']:
-#    print i
+def getPoem(sc):
+    print "Getting poem..."
+
+    httpreq = urllib2.urlopen(url)
+    response = httpreq.read()
+    poem = json.loads(response)
+
+    line = quote(poem["poem"][0], safe='')
+    print "Sending line: " + line
+
+    s.enter(poemRequestTime, 1, getPoem, (sc,))
+
+# Start the scheduler
+s.enter(poemRequestTime, 1, getPoem, (s,))
+s.run()
 
 
-line = quote(poem["poem"][0], safe='')
-print "Sending line: " + line
-
-sendURL = 'http://192.168.125.47/small/2/2/' + line
-
-signReq = urllib2.urlopen(sendURL)
-signReq.read()
+#sendURL = 'http://192.168.125.47/small/2/2/' + line
+#signReq = urllib2.urlopen(sendURL)
+#signReq.read()
